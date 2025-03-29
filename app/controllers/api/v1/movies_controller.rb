3f3a803b0f748b5788ce6dc67c64e7de06f8eeb9
Joseph Bloom <jsphbloom@gmail.com>
2025-03-29 13:53:28 -0600

    added conditional if statement to movies index method depending on present params. Added movie_search method to gateway

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::MoviesController < ApplicationController
 
   def index
-    movies = MovieGateway.top_rated_movies
+
+    movies = if params.present?
+      MovieGateway.movie_search(params)
+    else
+      MovieGateway.top_rated_movies
+    end
+    
     render json: MovieSerializer.format_movie_list(movies)
   end
 end
--- app/gateways/movie_gateway.rb
+++ app/gateways/movie_gateway.rb
@@ -1,7 +1,17 @@
 class MovieGateway
 
   def self.top_rated_movies
-    response = conn.get("/3/movie/top_rated")
+    response = conn.get("/3/movie/top_rated") do |request|
+      request.params["page"] = 1
+    end
+    JSON.parse(response.body, symbolize_names: true)[:results]
+  end
+
+  def self.movie_search(params)
+    response = conn.get("/3/discover/movie") do |request|
+      params.each { |key, value| request.params[key] = value }
+      request.params["page"] = 1
+    end
     JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
