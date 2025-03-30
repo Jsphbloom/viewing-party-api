00fbd2493725ae709c294ab34b12748e841915c1
Joseph Bloom <jsphbloom@gmail.com>
2025-03-30 10:51:25 -0600

    can create a viewing party, 200OK in postman. invitees still an empty array.

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::ViewingPartiesController < ApplicationController
 
     if party.save
       create_invitees(party, params[:invitees])
-      render json: ViewingPartySerializer.format_viewing_party(party)
+      render json: ViewingPartySerializer.new(party)
     else
       render json: { errors: party.errors.full_messages }, status: :unprocessable_entity
     end
@@ -14,13 +14,13 @@ class Api::V1::ViewingPartiesController < ApplicationController
   private
 
   def viewing_party_params
-    params.permit(:name, :start_time, :end_time, :movie_id, :movie_title)
+    params.permit(:name, :start_time, :end_time, :movie_id, :movie_title, :host_id)
   end
 
   def create_invitees(party, invitees)
     invitees.each do |invitee|
-      user = User.find_by(id: user_id)
-      party.viewing_party_invitees.create(user: user)
+      user = User.find_by(id: invitee)
+      party.viewing_party_invitees.create(user: user) if user
     end
   end
 end
--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,6 +1,6 @@
 class ViewingParty < ApplicationRecord
-  belongs_to :host, class_name: 'User'
-  has_many :viewing_party_attendees
+  belongs_to :host, class_name: 'User', foreign_key: 'host_id'
+  has_many :viewing_party_invitees
   has_many :invitees, through: :viewing_party_invitees
 
   validates :name, :start_time, :end_time, :movie_id, :movie_title, presence: true
