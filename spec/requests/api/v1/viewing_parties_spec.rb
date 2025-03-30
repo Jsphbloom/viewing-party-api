530c0b86e3c07472d22e08e6378c7e47f6ba5519
Joseph Bloom <jsphbloom@gmail.com>
2025-03-30 11:54:24 -0600

    fleshed out test, expected results upon viewing party creation

--- spec/requests/api/v1/viewing_parties_spec.rb
+++ spec/requests/api/v1/viewing_parties_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "ViewingParties", type: :request do
   describe "POST a viewing party" do
     it "can create a viewing party" do
-      create(:user, id: 3)
-      create(:user, id: 5)
-      create(:user, id: 11)
-      create(:user, id: 7)
+      @host = create(:user, id: 3)
+      @user1 = create(:user, id: 5)
+      @user2 = create(:user, id: 11)
+      @user3 = create(:user, id: 7)
 
-      party = create(:viewing_party)
+      party = create(:viewing_party, invitees: [@user1, @user2, @user3])
 
       headers = {"CONTENT_TYPE" => "application/json"}
 
@@ -25,6 +25,15 @@ RSpec.describe "ViewingParties", type: :request do
 
       post "/api/v1/viewing_parties", headers: headers, params: JSON.generate(params)
       expect(response).to have_http_status(:ok)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data][:type]).to eq("viewing_party")
+      expect(json[:data][:id]).to be_a(String)
+      expect(json[:data][:attributes]).to have_key(:movie_title)
+      expect(json[:data][:attributes]).to have_key(:name)
+      expect(json[:data][:attributes][:invitees][0]).to be_a(Hash)
+      expect(json[:data][:attributes][:invitees][0][:id]).to eq(5)
+      expect(json[:data][:attributes][:host_id]).to eq(@host.id)
 
     end
   end
