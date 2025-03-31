d7091b1d8497ac993074e83019b51af69aff7ac2
Joseph Bloom <jsphbloom@gmail.com>
2025-03-31 12:09:35 -0600

    postman retrieving single user. serializer set up and correctly formatting responses. added route for user#show. added show method to user controller.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -16,4 +16,36 @@ class UserSerializer
         end
     }
   end
+
+  def self.format_single_user(user)
+    {
+      id: user.id.to_s,
+      type: "user",
+      attributes: {
+        name: user.name,
+        username: user.username,
+        viewing_parties_hosted: user.hosted_parties.map do |party|
+          {
+            id: party.id,
+          name: party.name,
+          start_time: party.start_time.strftime('%Y-%m-%d %H:%M:%S'),
+          end_time: party.end_time.strftime('%Y-%m-%d %H:%M:%S'),
+          movie_id: party.movie_id,
+          movie_title: party.movie_title,
+          host_id: party.host_id
+          }
+        end,
+        viewing_parties_invited: user.viewing_parties.map do |party|
+          {
+          name: party.name,
+          start_time: party.start_time.strftime('%Y-%m-%d %H:%M:%S'),
+          end_time: party.end_time.strftime('%Y-%m-%d %H:%M:%S'),
+          movie_id: party.movie_id,
+          movie_title: party.movie.title,
+          host_id: party.host_id
+          }
+        end
+      }
+    }
+  end
 end
